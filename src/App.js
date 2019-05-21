7d0da9f6657d27dc295d3f0d9430faf46f33f8f9
Slava <zakharovvu@gmail.com>
2019-05-21 21:19:17 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0
+  }
+  numberTimer = null
+
+  componentWillUnmount() {
+    clearTimeout(this.numberTimer)
+  }
+
+  handleClick = () => {
+    clearTimeout(this.numberTimer)
+
+    let ms = 0 
+    const maxNumber = 100000 
+    let random = Math.floor(Math.random() * maxNumber) 
+    const stepDown = Math.round(random / 100) || 1 
+    const stepMs = 10 
+    const startSlow = random * 0.2 
+    const bindingThis = this 
+
+    this.numberTimer = setTimeout(function teak() {
+      random -= stepDown
+      if (random < 0) {
+        clearTimeout(this.numberTimer)
+        bindingThis.setState({ count: 0 })
+      } else {
+        if (random < startSlow) ms += stepMs
+        
+        clearTimeout(this.numberTimer)
+        this.numberTimer = setTimeout(teak, ms)
+        bindingThis.setState({ count: random })
+    }}, ms);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>{this.state.count}</div>
+        <button onClick={this.handleClick}>Click</button>
+      </div>
+    )
+  }
 }
 
 export default App;
